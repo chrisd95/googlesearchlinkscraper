70b4639bec105ddeda08b45ea67c9b142c68a910
chrisd95 <34041303+chrisd95@users.noreply.github.com>
2021-03-30 17:58:21 -0400

    Update googlequeryscrape_v2.py

--- googlequeryscrape_v2.py
+++ googlequeryscrape_v2.py
@@ -5,14 +5,14 @@ from googlesearch import search
 query_depth = 100
 
 # Input queries from csv
-with open('queries.csv', newline='') as file:
+with open('./input/queries.csv', newline='') as file:
     reader = csv.reader(file)
     queries = list(reader)
 
 
 for query in queries:
     # Output files
-    with open(query[0] + ".csv", 'a',  newline='') as search_results_file:
+    with open("./output/" + query[0] + ".csv", 'a',  newline='') as search_results_file:
         writer = csv.writer(search_results_file)
         for i in search(query[0], tld="com", num=query_depth, stop=query_depth, pause=2):
             writer.writerow([i])
