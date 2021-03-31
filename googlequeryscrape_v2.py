a4cf25e14bf780dacbcf1bf321bdd4fb70418667
Chris <christopher.dip@umontreal.ca>
2021-03-30 20:11:44 -0400

    Fixed results per page and total results

--- googlequeryscrape_v2.py
+++ googlequeryscrape_v2.py
@@ -2,7 +2,13 @@ import csv
 from googlesearch import search
 
 # Query parameters
-query_depth = 100
+
+# From 1 to 100
+results_per_page = 100
+
+# From 1 to X
+total_results = 1000
+
 
 # Input queries from csv
 with open('./input/queries.csv', newline='') as file:
@@ -14,5 +20,6 @@ for query in queries:
     # Output files
     with open("./output/" + query[0] + ".csv", 'a',  newline='') as search_results_file:
         writer = csv.writer(search_results_file)
-        for i in search(query[0], tld="com", num=query_depth, stop=query_depth, pause=2):
-            writer.writerow([i])
+        for i in range(total_results // results_per_page):
+            for j in search(query[0], tld="com", num=results_per_page, stop=None, start=i*results_per_page, pause=10):
+                writer.writerow([j])
